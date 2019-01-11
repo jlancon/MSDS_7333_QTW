b118c843449066d9e939e61fd85274dedc5c619f
David Stroud <david@davidstroud.me>
2019-01-10 19:23:43 -0500

    Made separate file for Stroud work

--- CaseStudy_2/Stroud_Predicting_Location.R
+++ CaseStudy_2/Stroud_Predicting_Location.R
@@ -1,7 +1,1 @@
-refs = seq(0, by = 45, length  = 9)
-angles = 120
-q = sapply(angles, function(z) which.min(abs(z - refs)))
-typeof(q)
-c(refs[1:8], 0)[q]
-typeof(q)
-q
+matrix(1:9, byrow = TRUE, nrow = 3)
